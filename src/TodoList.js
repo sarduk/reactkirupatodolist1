9077d0998126f8d0f1277b6816d84a2b7b365c6c
Rea Biagio <reabiagio@gmail.com>
2020-08-26 12:51:05 +0200

    conclusa la messa in opera del codice di esempio del tutorial

---
+++ src/TodoList.js
@@ -0,0 +1,72 @@
+import "./TodoList.css";
+
+import React, { Component } from "react";
+import TodoItems from "./TodoItems";
+
+
+class TodoList extends Component {
+
+    constructor(props) {
+        super(props);
+
+        //oggettto che rappresenta lo stato dell'applicazione
+        this.state = {
+            items: []
+          };
+
+        //override dei metodi per mantenere il riferimento a this
+        this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+      }
+       
+    addItem(e) {
+      if (this._inputElement.value !== "") {
+        var newItem = {
+          text: this._inputElement.value,
+          key: Date.now()
+        };
+        this.setState((prevState) => {
+          return {
+            items: prevState.items.concat(newItem)
+          };
+        });
+        this._inputElement.value = "";
+      }
+      e.preventDefault();
+      console.log(this.state.items);
+    }
+        
+
+    deleteItem(key) {
+      var filteredItems = this.state.items.filter(function (item) {
+        return (item.key !== key);
+      });
+    
+      this.setState({
+        items: filteredItems
+      });
+    }
+    
+
+
+  render() {
+    return (
+<div className="todoListMain">
+<div className="header">
+<form onSubmit={this.addItem}>
+<input ref={(a) => this._inputElement = a} placeholder="enter task">
+</input>
+<button type="submit">add</button>
+</form>
+</div>
+<TodoItems entries={this.state.items} delete={this.deleteItem} />
+</div>
+    );
+  }
+}
+ 
+
+export default TodoList;
+
+
+
